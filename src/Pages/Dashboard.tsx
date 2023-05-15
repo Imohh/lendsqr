c015708b33ebba39a742add6396c45c88a2ee864
Imoh Precious <jaypee88830@gmail.com>
2023-05-16 00:18:49 +0100

    login form validation

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -29,6 +29,7 @@ const Dashboard = () => {
 	const url = "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
 
 	const [data, setData] = useState<User[]>([])
+	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState({})
 	const [show, setShow] = useState<boolean>(false)
 	const [searchTerm, setSearchTerm] = useState<string>('');
@@ -38,7 +39,12 @@ const Dashboard = () => {
   const itemsPerPage = 10; // Number of items to display per page
 
   useEffect(() => {
-    fetchData();
+    // fetchData();
+
+  	setTimeout(() => {
+      fetchData();
+    }, 1000);
+
   }, []);
 
   const fetchData = async () => {
@@ -46,8 +52,10 @@ const Dashboard = () => {
       const response = await fetch('https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users');
       const jsonData = await response.json();
       setData(jsonData);
+      setLoading(false);
     } catch (error) {
       console.log('Error fetching data:', error);
+      setLoading(false)
     }
   };
 
@@ -58,55 +66,24 @@ const Dashboard = () => {
   };
 
   const getStatus = (date: string): string => {
-	  const currentDate = new Date();
-	  const year = currentDate.getFullYear();
-	  const tenYearsAgo = year - 10;
-	  const year2000 = 2000;
-	  const year2022 = 2022;
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const tenYearsAgo = year - 10;
+  const year2000 = 2000;
+  const year2022 = 2022;
 
-	  const parsedDate = new Date(date);
+  const parsedDate = new Date(date);
 
-	  if (parsedDate.getFullYear() < year2000) {
-	    return 'blacklisted';
-	  } else if (parsedDate.getFullYear() >= year2022) {
-	    return 'active';
-	  } else if (parsedDate.getFullYear() <= tenYearsAgo) {
-	    return 'pending';
-	  }
+  if (parsedDate.getFullYear() < year2000) {
+    return 'blacklisted';
+  } else if (parsedDate.getFullYear() >= year2022) {
+    return 'active';
+  } else if (parsedDate.getFullYear() <= tenYearsAgo) {
+    return 'pending';
+  }
 
-	  return 'unknown';
-
-	    if ('active') {
-	      return 'red';
-	    } else if ('inactive') {
-	      return 'blue';
-	    } else if ('pending') {
-	      return 'green';
-	    }
-	    return 'black'; // Default color if status doesn't match any condition
-
-	};
-
-  // status conditional styling 
-  // const statusColor = status === 'Active' ? '#39CD62' : status === 'Pending' ? '#E9B200' : status === 'Inactive' ? '#545F7D' : status === 'Blacklisted' ? '#E4033B';
-  // let textColor: string;
-
-  // if (status === 'Inactive') {
-  //   textColor = '#545F7D';
-  // } else if (status === 'Active') {
-  //   textColor = '#39CD62';
-  // } else if (status === 'Blacklisted') {
-  //   textColor = '#E4033B';
-  // } else if (status === 'Pending') {
-  //   textColor = '#E9B200';
-  // } else {
-  //   textColor = 'inherit'; // Fallback color if no condition is met
-  // }
-
-  // const textStyle = {
-  //   color: textColor,
-  // };
-
+  return 'unknown';
+};
   const handleSearch = (term: string) => {
     setSearchTerm(term);
   };
@@ -117,28 +94,19 @@ const Dashboard = () => {
     user.orgName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
- // const handlePageChange = (page: number) => {
- //    setCurrentPage(page);
- //  };
-
- //  const renderPagination = () => {
- //    const pages = Array.from(Array(totalPages).keys()); // Generate an array of page numbers
-
- //    return (
- //      <div>
- //        {pages.map((page) => (
- //          <button key={page} onClick={() => handlePageChange(page + 1)}>
- //            {page + 1}
- //          </button>
- //        ))}
- //      </div>
- //    );
- //  };
-
 	const shouldShowSearch = true;
 
 
-	  
+	  const getStatusColor = (status: string): string => {
+	    if (status === 'Active') {
+	      return 'red';
+	    } else if (status === 'inactive') {
+	      return 'blue';
+	    } else if (status === 'pending') {
+	      return 'green';
+	    }
+	    return 'black'; // Default color if status doesn't match any condition
+	  };
 
 
 
@@ -200,63 +168,65 @@ const Dashboard = () => {
 		      </div>
 
 
-		      <section className="table-section">
-			    <table className="table">
-				  <thead>
-				      <th scope="col">organization</th>
-				      <th scope="col">username</th>
-				      <th scope="col">email</th>
-				      <th scope="col">phone number</th>
-				      <th scope="col">date joined</th>
-				      <th scope="col">status</th>
-				      <th></th>
-				  </thead>
+		      	<section className="table-section">
+				    {loading ? (
+				       	<div>Loading...</div> // Render the loader when loading is true
+				   	) : (
+				    <table className="table">
+					  <thead>
+					      <th scope="col">organization</th>
+					      <th scope="col">username</th>
+					      <th scope="col">email</th>
+					      <th scope="col">phone number</th>
+					      <th scope="col">date joined</th>
+					      <th scope="col">status</th>
+					      <th></th>
+					  </thead>
 
-				  {filteredUsers.map((item, index) => {
-        			return (
-					  <tbody key={item.id}>
-					    <tr >
-					      <td>{item.orgName}</td>
-					      <td>{item.userName}</td>
-					      <td>{item.email}</td>
-					      <td>{item.phoneNumber}</td>
-					      <td>{formatTime(item.createdAt)}</td>
-					      <td><span className="badge badge-pill">{getStatus(item.lastActiveDate)}</span></td>
-					      <td>
+					  {filteredUsers.map((item, index) => {
+	        			return (
+						  <tbody key={item.id}>
+						    <tr >
+						      <td>{item.orgName}</td>
+						      <td>{item.userName}</td>
+						      <td>{item.email}</td>
+						      <td>{item.phoneNumber}</td>
+						      <td>{formatTime(item.createdAt)}</td>
+						      <td><span className="badge badge-pill" style={{ color: getStatusColor(item.lastActiveDate) }} >{getStatus(item.lastActiveDate)}</span></td>
+						      <td>
 
-					      	<div id="container">
-							    <div id="menu-wrap">
-							      <input type="checkbox" className="toggler" />
-							      <div className="dots">
-							        <div></div>
-							      </div>
-							      <div className="menu">
-							        <div>
-							          <ul>
-							            <li>
-								            <Link data-item-id={item.id} to={`/dashboard/users/${item.id}`} key={item.id}>
-							            		<a href="#" className="link">
-									            	<img src={viewDetail} />view details
-									            </a>
-								            </Link>
-									    </li>
-							            <li><a href="#" className="link"><img src={blacklistUser} />blacklist user</a></li>
-							            <li><a href="#" className="link"><img src={activateUser} />activate user</a></li>
-							          </ul>
-							        </div>
-							      </div>
-							    </div>
-							</div>
+						      	<div id="container">
+								    <div id="menu-wrap">
+								      <input type="checkbox" className="toggler" />
+								      <div className="dots">
+								        <div></div>
+								      </div>
+								      <div className="menu">
+								        <div>
+								          <ul>
+								            <li>
+									            <Link data-item-id={item.id} to={`/dashboard/users/${item.id}`} key={item.id}>
+								            		<a href="#" className="link">
+										            	<img src={viewDetail} />view details
+										            </a>
+									            </Link>
+										    </li>
+								            <li><a href="#" className="link"><img src={blacklistUser} />blacklist user</a></li>
+								            <li><a href="#" className="link"><img src={activateUser} />activate user</a></li>
+								          </ul>
+								        </div>
+								      </div>
+								    </div>
+								</div>
 
-					      </td>				      
-					  	</tr>
-				  	  </tbody>
-				  )
-		          })}
-				</table>
-
-				{/*{renderPagination()}*/}
-			  </section>
+						      </td>				      
+						  	</tr>
+					  	  </tbody>
+					  )
+			          })}
+					</table>
+					)}	
+			  	  </section>
 
 
 
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,22 +1,75 @@
 import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import logo from '../images/logo.svg'
 import loginImage from '../images/login-image.svg'
 import '../styles/login.scss'
 
+
 function Login() {
 
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  
 
+  const navigate = useNavigate();
+
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (!email) {
+      setEmailError('Email is required');
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError('Invalid email address');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (!password) {
+      setPasswordError('Password is required');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (validateForm()) {
+      // Perform form submission or other actions
+      console.log('Form submitted successfully');
+
+      // Navigate to the success page
+      navigate('/dashboard');
+    }
+  };
+
+
+
+  // hide and show password
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
 
   return (
     <HelmetProvider>
@@ -52,10 +105,16 @@ function Login() {
               <div className="login-form">
                 <p className="welcome-text">welcome!</p>
                 <p className="sub-text">enter details to login</p>
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="row">
                     <div className="col-lg-12">
-                      <input type="text" placeholder="Email" />
+                      <input 
+                        type="text" 
+                        placeholder="Email" 
+                        value={email}
+                        onChange={handleEmailChange}
+                      />
+                      {emailError && <p className="error">{emailError}</p>}
                     </div>
                   </div>
                   <div className="row">
@@ -69,10 +128,11 @@ function Login() {
                         id="subdomain"
                       />
                       <input onClick={toggleShowPassword} type="button" id="showText" value={showPassword ? 'Hide' : 'Show'}/>
+                      {passwordError && <p className="error">{passwordError}</p>}
                     </div>
                   </div>
                   <p className="forgot-text">forgot password?</p>
-                  <button>log in</button>
+                  <button type="submit">log in</button>
                 </form>
               </div>
             </div>
