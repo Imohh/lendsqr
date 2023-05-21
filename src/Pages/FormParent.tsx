089631f83cf3eb5c1bd7d92aadc1510f5980afa8
Imoh Precious <jaypee88830@gmail.com>
2023-05-21 16:09:08 +0100

    new commit

---
+++ src/Pages/FormParent.tsx
@@ -0,0 +1,20 @@
+// const FormParent: React.FC = () => {
+//   const history = useHistory();
+//   const location = useLocation();
+//   const { pathname } = location;
+
+//   const renderChildComponent = () => {
+//     switch (pathname) {
+//       case '/step1':
+//         return <Step1 />;
+//       case '/step2':
+//         return <Step2 />;
+//       default:
+//         return null;
+//     }
+//   };
+
+//   return <div>{renderChildComponent()}</div>;
+// };
+
+// export default FormParent;
